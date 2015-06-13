151103e542ef0e1ec9348620b63bff6a36dc8b7d
Josh Chappelle <jchappelle007@gmail.com>
2015-06-12 23:25:01 -0500

    Added all wool variants

--- jcraft-core/src/main/java/com/chappelle/jcraft/Block.java
+++ jcraft-core/src/main/java/com/chappelle/jcraft/Block.java
@@ -117,6 +117,22 @@ public class Block
 	public static final Block mushroomBrown = new BlockFlower(17, 13, 1);
 	public static final Block mushroomRed = new BlockFlower(18, 12, 1);
 	public static final Block water = new BlockWater(19);
+	public static final Block woolWhite = new BlockWool(20, 0, 4);
+	public static final Block woolBlack = new BlockWool(21, 1, 7);
+	public static final Block woolRed = new BlockWool(22, 1, 8);
+	public static final Block woolGreen = new BlockWool(23, 1, 9);
+	public static final Block woolBrown = new BlockWool(24, 1, 10);
+	public static final Block woolBlue = new BlockWool(25, 1, 11);
+	public static final Block woolPurple = new BlockWool(26, 1, 12);
+	public static final Block woolCyan = new BlockWool(27, 1, 13);
+	public static final Block woolSilver = new BlockWool(28, 1, 14);
+	public static final Block woolGray = new BlockWool(29, 2, 7);
+	public static final Block woolPink = new BlockWool(30, 2, 8);
+	public static final Block woolLime = new BlockWool(31, 2, 9);
+	public static final Block woolYellow = new BlockWool(32, 2, 10);
+	public static final Block woolLightBlue = new BlockWool(33, 2, 11);
+	public static final Block woolMagenta = new BlockWool(34, 2, 12);
+	public static final Block woolOrange = new BlockWool(35, 2, 13);
 	
 	private BlockShape[] shapes = new BlockShape[] { new BlockShape_Cube() };
 	private BlockSkin[] skins;
---
+++ jcraft-core/src/main/java/com/chappelle/jcraft/blocks/BlockWool.java
@@ -0,0 +1,28 @@
+package com.chappelle.jcraft.blocks;
+
+import com.chappelle.jcraft.Block;
+import com.chappelle.jcraft.BlockSkin;
+import com.chappelle.jcraft.BlockSkin_TextureLocation;
+import com.chappelle.jcraft.Vector3Int;
+import com.chappelle.jcraft.World;
+import com.jme3.math.Vector3f;
+
+public class BlockWool extends Block
+{
+	public BlockWool(int blockId, int textureCol, int textureRow)
+	{
+		super(blockId, new BlockSkin[] { new BlockSkin(new BlockSkin_TextureLocation(textureCol, textureRow), false) });
+	}
+
+	public void onBlockPlaced(World world, Vector3Int location, Vector3f contactNormal, Vector3f cameraDirectionAsUnitVector)
+	{
+	}
+
+	public void onBlockRemoved(World world, Vector3Int location)
+	{
+	}
+
+	public void onEntityWalking(World world, Vector3Int location)
+	{
+	}
+}
