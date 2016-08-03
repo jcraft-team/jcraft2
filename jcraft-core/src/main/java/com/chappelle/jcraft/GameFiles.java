ca2c6fa1bd689a0783758bbf6b7e1d38114aec6b
jchappelle <jchappelle007@gmail.com>
2016-08-02 22:01:34 -0500

    Added GameFiles

---
+++ jcraft-core/src/main/java/com/chappelle/jcraft/GameFiles.java
@@ -0,0 +1,33 @@
+package com.chappelle.jcraft;
+
+import java.io.File;
+
+import org.apache.commons.lang3.SystemUtils;
+
+public class GameFiles
+{
+	static
+	{
+		File dataDir = getDataDir();
+		if(!dataDir.exists())
+		{
+			dataDir.mkdirs();
+		}
+	}
+	/**
+	 * Returns the root folder for which user specific data will be stored. Game saves, options...etc.
+	 * @return the root folder for which user specific data will be stored
+	 */
+    public static File getDataDir() 
+    {
+    	if(SystemUtils.IS_OS_WINDOWS)
+    	{
+    		return new File(System.getenv("APPDATA"), ".jcraft");
+    	}
+    	else
+    	{
+    		return new File(SystemUtils.getUserHome(), ".jcraft");
+    	}
+    }
+
+}
