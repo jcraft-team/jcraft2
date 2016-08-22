6b65a227a2cc0b884bbbb96c20dbcb00e2237e89
jchappelle <jchappelle007@gmail.com>
2016-08-21 19:29:22 -0500

    Reworked crosshairs to use a PNG

--- jcraft-jme3/src/main/java/com/chappelle/jcraft/jme3/CrosshairsControl.java
+++ jcraft-jme3/src/main/java/com/chappelle/jcraft/jme3/CrosshairsControl.java
@@ -2,35 +2,36 @@ package com.chappelle.jcraft.jme3;
 
 import com.chappelle.jcraft.EntityPlayer;
 import com.jme3.app.SimpleApplication;
-import com.jme3.font.*;
 import com.jme3.renderer.*;
 import com.jme3.scene.*;
 import com.jme3.scene.control.AbstractControl;
 import com.jme3.system.AppSettings;
+import com.jme3.ui.Picture;
 
 public class CrosshairsControl extends AbstractControl
 {
 	private Node debugNode;
 	private Node guiNode;
-	private BitmapText crosshairs;
 	private AppSettings settings;
-	private BitmapFont guiFont;
-	private float width;
-	private float height;
+	private float screenWidth;
+	private float screenHeight;
+	private Picture plus;
+	
+	private static final int WIDTH = 30;
+	private static final int HEIGHT = 30;
 	
 	public CrosshairsControl(SimpleApplication app, AppSettings appSettings, EntityPlayer player)
 	{
 		this.debugNode = new Node("crosshairs");
 		this.guiNode = app.getGuiNode();
 		this.settings = appSettings;
-		this.guiFont = app.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+		plus = new Picture("icons");
+		plus.setWidth(WIDTH);
+		plus.setHeight(HEIGHT);
+		plus.setImage(app.getAssetManager(), "Textures/gui/crosshairs.png", true);
 		
-		width = settings.getWidth();
-		height = settings.getHeight();
-		
-        crosshairs = new BitmapText(guiFont, false);
-        crosshairs.setSize(guiFont.getCharSet().getRenderedSize() * 2);
-        crosshairs.setText("+");
+		screenWidth = settings.getWidth();
+		screenHeight = settings.getHeight();
 	}
 	
 	@Override
@@ -38,32 +39,31 @@ public class CrosshairsControl extends AbstractControl
 	{
 		if(spatial == null)
 		{
-			guiNode.detachChild(crosshairs);
+			guiNode.detachChild(plus);
 			guiNode.detachChild(debugNode);
 		}
 		else if (spatial instanceof Node)
         {
             guiNode.attachChild(debugNode);//TODO: Investigate what this node is for
-            guiNode.attachChild(crosshairs);
-
+            guiNode.attachChild(plus);
             positionElements();
         }	
 	}
 
 	public void positionElements()
 	{
-        float x = settings.getWidth() / 2 - guiFont.getCharSet().getRenderedSize() / 3 * 2;
-        float y = settings.getHeight() / 2 + crosshairs.getLineHeight() / 2;
-        crosshairs.setLocalTranslation(x, y, 0);
+        float x = screenWidth / 2 - (WIDTH/2);
+        float y = screenHeight / 2 - (HEIGHT/2);
+        plus.setLocalTranslation(x, y, 0);
 	}
 	
 	@Override
 	protected void controlUpdate(float tpf)
 	{
-		if(settings.getWidth() != width || settings.getHeight() != height)//May have toggled fullscreen mode
+		if(settings.getWidth() != screenWidth || settings.getHeight() != screenHeight)//May have toggled fullscreen mode
 		{
-			width = settings.getWidth();
-			height = settings.getHeight();
+			screenWidth = settings.getWidth();
+			screenHeight = settings.getHeight();
 
 			positionElements();
 		}
